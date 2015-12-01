3dd466b4b64d9ea3fbccc7d374aec19b98a21584
samsung <a.partitskiy@gmail.com>
2015-12-01 17:16:01 +0200

    first commit

---
+++ js/application.js
@@ -0,0 +1,21 @@
+$(function() {
+  $('.nav li').on('click', function() {
+    $(this).parent().find('li.active').removeClass('active');
+    $(this).addClass('active');
+  });
+
+  $('.carousel').carousel({
+    interval: 3000
+  }, 'pause');
+
+  $('.body div').hide();
+  var li = $('.navbar .nav.navbar-nav li');
+
+  $("#tablesorter").tablesorter();
+
+  var options = {
+    valueNames: [ 'name', 'price' ]
+  };
+
+  var userList = new List('price-list', options);
+});
